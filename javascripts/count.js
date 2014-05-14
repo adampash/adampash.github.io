87521c8dd742c47ceac48fb1391fd452f5ff41bc
Adam Pash <adam.pash@gmail.com>
2014-05-14 12:22:23 -0400

    Only one success function

--- javascripts/count.js
+++ javascripts/count.js
@@ -7,13 +7,9 @@
       type: 'PUT',
       data: {url: window.location.href}
     }
-  ).done(function(data) {
-    console.log(data);
-  }
   ).error(function(data) {
     console.log('error');
-  }
-  ).success(function(data) {
+  }).success(function(data) {
     console.log(data);
   });
 
